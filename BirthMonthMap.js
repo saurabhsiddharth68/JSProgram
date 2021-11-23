b1b1d6e9b201c18ce7e07ee8b9f037946b1ab0f1
Saurabh Siddharth <saurabhsiddharth68>
2021-11-23 23:59:34 +0530

    Find Same borth Month using Map

---
+++ BirthMonthMap.js
@@ -0,0 +1,13 @@
+let personBirthmonth = new Map();
+for(let person = 1; person<=50 ; person++)
+{
+    let birthmonth = (Math.floor(Math.random() * 100) % 12) + 1;
+    personBirthmonth.set(person, birthmonth);
+}
+for(let month = 1; month <= 12; month++)
+{
+    let sameMonth = [...personBirthmonth.entries()].filter(([a,b]) => b == month).map(([c,d]) => c);
+    console.log("People with birthday in month " + month);
+    console.log(sameMonth);
+    console.log("Printing 1992 to 1993");
+}
